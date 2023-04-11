b30181bf206f3d566094937ed195f699ad6bfb7d
jespinol <76534045+jespinol@users.noreply.github.com>
2023-04-10 23:51:22 -0400

    user can indicate if the data has increasing or decreasing concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import re
 
 import numpy as np
 
@@ -9,19 +10,52 @@ from modules.save_xlsx import output_results
 
 
 def main():
-    print(
-        "Program assumes ONE set of donor+ and donor- per file and ONE well without acceptor (which is at the last position)")
-    dataset_info = get_dataset_info()
-    raw_signal = open_and_parse_dataset(dataset_info[PATH])
+    print("Program requires csv file(s) with single replicates arranged in a specific format. See README")
+    while True:
+        dataProcess(get_dataset_info())
+        print(f"\n{'*' * 50}\n")
 
-    corrected_signal = {SIGNAL_VALUES: process_signal(raw_signal)}
+
+def get_dataset_info():
+    output = {PATH: input("Enter path or 'q' to exit: ") or DEFAULT_PATH,
+              MAX_CONC: 0,
+              DIL_FACTOR: 0}
+
+    if output[PATH] == "q":
+        quit()
+
+    while True:
+        try:
+            output[MAX_CONC] = int(
+                input("Enter [max] in mM (or press enter for '10mM'): ") or DEFAULT_MAX_CONC)
+            break
+        except ValueError:
+            print("Invalid input type, please enter an integer value.")
+
+    output[CONC_REVERSE] = re.search(r"[yY]", input("Are the concentrations in increasing order? [y/Y] "))
+
+    while True:
+        try:
+            output[DIL_FACTOR] = int(
+                input("Enter dilution factor (or press enter for '2'): ") or DEFAULT_DIL_FACTOR)
+            break
+        except ValueError:
+            print("Invalid input type, please enter an integer value.")
+
+    return output
+
+
+def dataProcess(dataset_info):
+    raw_signal = parse_dataset(dataset_info[PATH])
+
+    corrected_signal = {SIGNAL_VALUES: format_raw_signal(raw_signal)}
     normalized_signal = {SIGNAL_VALUES: normalize_signal(corrected_signal)}
 
     dataset_info[NUM_REPEATS] = len(corrected_signal[SIGNAL_VALUES])
     dataset_info[NUM_DATAPOINTS] = len(next(iter(corrected_signal[SIGNAL_VALUES].values())))
 
     corrected_signal[CONC] = normalized_signal[CONC] = calculate_concentrations(dataset_info)
-    corrected_signal[LOG_CONC] = normalized_signal[LOG_CONC] = convert_conc_log(corrected_signal[CONC])
+    corrected_signal[LOG_CONC] = normalized_signal[LOG_CONC] = convert_conc_to_log(corrected_signal[CONC])
 
     corrected_signal[STATS] = calculate_statistics(corrected_signal)
     normalized_signal[STATS] = calculate_statistics(normalized_signal)
@@ -34,30 +68,7 @@ def main():
     return
 
 
-def get_dataset_info():
-    output = {PATH: input("Enter path (or press enter for default): ") or DEFAULT_PATH,
-              MAX_CONC: 0,
-              DIL_FACTOR: 0}
-
-    while True:
-        try:
-            output[MAX_CONC] = int(
-                input("Enter [max] in mM (or press enter for default): ") or DEFAULT_MAX_CONC)
-            break
-        except ValueError:
-            print("Invalid input type, please enter an integer value.")
-    while True:
-        try:
-            output[DIL_FACTOR] = int(
-                input("Enter dilution factor (or press enter for default): ") or DEFAULT_DIL_FACTOR)
-            break
-        except ValueError:
-            print("Invalid input type, please enter an integer value.")
-
-    return output
-
-
-def open_and_parse_dataset(path):
+def parse_dataset(path):
     raw_data = {"615": {}, "665": {}}
     try:
         # check if input is a directory
@@ -68,33 +79,33 @@ def open_and_parse_dataset(path):
                     with open(os.path.join(path, file), "r") as f:
                         reader = csv.reader(f)
                         line = next(reader)
-                        raw_data["615"][len(raw_data["615"]) + 1] = parse_data(reader, line, True)
+                        raw_data["615"][len(raw_data["615"]) + 1] = parse_values(reader, line, True)
                         line = next(reader)
-                        raw_data["665"][len(raw_data["665"]) + 1] = parse_data(reader, line, False)
+                        raw_data["665"][len(raw_data["665"]) + 1] = parse_values(reader, line, False)
         # if not a directory, assume it is a file
         else:
             with open(path, "r") as file:
                 reader = csv.reader(file)
                 line = next(reader)
-                raw_data["615"][1] = parse_data(reader, line, True)
+                raw_data["615"][1] = parse_values(reader, line, True)
                 line = next(reader)
-                raw_data["665"][1] = parse_data(reader, line, False)
+                raw_data["665"][1] = parse_values(reader, line, False)
     except IOError:
         print("Input directory/file does not exist or does not contain a valid csv file.")
 
     return raw_data
 
 
-def parse_data(reader, line, is_first_half):
+def parse_values(reader, line, is_first_half):
     data_arr = []
-    while (line != []) if is_first_half else (line is not None):
+    while line:
         data_arr.append(int(line[0]))
         line = next(reader, None)
 
     return data_arr
 
 
-def process_signal(data):
+def format_raw_signal(data):
     output = {}
     for i in range(1, len(data["665"]) + 1):
         output[i] = correct_signal(data["615"][i], data["665"][i])
@@ -134,13 +145,16 @@ def calculate_concentrations(data_info):
     concentrations = []
     current_concentration = float(max_concentration) * 1000
     for i in range(1, num_datapoints + 1):
-        concentrations.append(current_concentration)
+        if data_info[CONC_REVERSE]:
+            concentrations.insert(0, current_concentration)
+        else:
+            concentrations.append(current_concentration)
         current_concentration /= dilution_factor
 
     return concentrations
 
 
-def convert_conc_log(array_of_concentrations):
+def convert_conc_to_log(array_of_concentrations):
     output = []
     for value in array_of_concentrations:
         output.append(np.log10(value))
