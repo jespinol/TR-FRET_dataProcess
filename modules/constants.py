6dc505a235dd295639c59dbe399b86cf7860e847
jespinol <76534045+jespinol@users.noreply.github.com>
2023-05-13 09:44:49 -0400

    added new constants and moved some variables to this file

--- modules/constants.py
+++ modules/constants.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # input file constant names and default values
 DEFAULT_PATH = "/Users/jespinol/Downloads/TR-FRET/trfret_data"
 PATH = "Path to file(s)"
@@ -5,6 +7,9 @@ DEFAULT_MAX_CONC = 10
 MAX_CONC = "Maximum concentration (µM)"
 DEFAULT_DIL_FACTOR = 2
 DIL_FACTOR = "Dilution factor"
+DEFAULT_COL_NUM = 6
+CHART_COL_WIDTH = 6
+CHART_ROW_HEIGHT = 27
 
 # constant names for replicate number and dataset length
 NUM_REPEATS = "Number of replicates"
@@ -20,10 +25,33 @@ AVERAGE_SIGNAL = "Average signal"
 STD_DEV = "Standard deviation"
 STD_ERR = "Standard error"
 
-# constant names for curve fitting values
+# constant names for curve fitting
+COOPERATIVE_MODEL = "Cooperative model"
 SIMPLE_MODEL = "Simple model"
 QUADRATIC_MODEL = "Quadratic model"
+MODELS = [COOPERATIVE_MODEL, SIMPLE_MODEL, QUADRATIC_MODEL]
+
+
+def simple_model_equation(lt, kd):
+    return lt / (kd + lt)
+
+
+def quadratic_model_equation(lt, kd, rt=1):
+    rl = ((rt + lt + kd) - np.sqrt(((rt + lt + kd) ** 2) - (4 * rt * lt))) / 2
+    return (lt - rl) / (kd + (lt - rl))
+
+
+def hill_equation(lt, ec50, hill_slope, bottom, top):
+    return bottom + ((top - bottom) * np.power(lt, hill_slope)) / (
+            np.power(ec50, hill_slope) + np.power(lt, hill_slope))
+
+
+MODEL_EQUATIONS = {COOPERATIVE_MODEL: hill_equation, SIMPLE_MODEL: simple_model_equation,
+                   QUADRATIC_MODEL: quadratic_model_equation}
 KD = "KD (nM)"
+NH = "nH"
+BOTTOM = "Bottom param"
+TOP = "Top param"
 CONF_INT = "Confidence interval"
 CONF_INT_LOWER = "CI lower bound"
 CONF_INT_UPPER = "CI upper bound"
@@ -31,11 +59,9 @@ STD_DEV_FIT = "Standard deviation"
 STD_ERR_FIT = "Standard error"
 
 # constant names related to output file
-NORMALIZED_WS = "Normalized"
-UNNORMALIZED_WS = "Unnormalized"
-DATAFRAME_FORMAT = [LOG_CONC, SIGNAL_VALUES, STATS]
-SELECTED_MODEL = SIMPLE_MODEL
-CALC_X = "Calc x"
-CALC_Y = "Calc y"
+WS_NAME = "Data"
+SIGNAL_DATAFRAME_FORMAT = [LOG_CONC, SIGNAL_VALUES, STATS]
+PARAMETER = "Parameter"
+VALUE = "Value"
 HELPER_X = "Helper x"
 HELPER_Y = "Helper y"
