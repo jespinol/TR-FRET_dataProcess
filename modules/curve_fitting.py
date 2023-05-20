5160a045e0ec01ec1535fc0573bb08c4c8b67de6
jespinol <76534045+jespinol@users.noreply.github.com>
2023-05-20 10:21:30 -0400

    added error handling for bad data/no binding

--- modules/curve_fitting.py
+++ modules/curve_fitting.py
@@ -9,19 +9,22 @@ def fit_curve(dataset_info, data, model):
     y_data = np.array(data[STATS][AVERAGE_SIGNAL])
     df = dataset_info[NUM_REPEATS]
 
-    popt, pcov = curve_fit(model, x_data, y_data)
-    kd = popt[0]
+    try:
+        popt, pcov = curve_fit(model, x_data, y_data)
+        kd = popt[0]
+        conf_int_low, conf_int_hi, std_dev, std_err = calculate_statistics_from_fit(kd, pcov, df)
 
-    conf_int_low, conf_int_hi, std_dev, std_err = calculate_statistics_from_fit(kd, pcov, df)
+        if model == hill_equation:
+            nH = popt[1]
+            bottom = popt[2]
+            top = popt[3]
+            return {KD: kd, NH: nH, BOTTOM: bottom, TOP: top, CONF_INT_LOWER: conf_int_low, CONF_INT_UPPER: conf_int_hi,
+                    STD_DEV: std_dev, STD_ERR: std_err}
 
-    if model == hill_equation:
-        nH = popt[1]
-        bottom = popt[2]
-        top = popt[3]
-        return {KD: kd, NH: nH, BOTTOM: bottom, TOP: top, CONF_INT_LOWER: conf_int_low, CONF_INT_UPPER: conf_int_hi,
-                STD_DEV: std_dev, STD_ERR: std_err}
-
-    return {KD: kd, CONF_INT_LOWER: conf_int_low, CONF_INT_UPPER: conf_int_hi, STD_DEV: std_dev, STD_ERR: std_err}
+        return {KD: kd, CONF_INT_LOWER: conf_int_low, CONF_INT_UPPER: conf_int_hi, STD_DEV: std_dev, STD_ERR: std_err}
+    except RuntimeError:
+        return {KD: 999, NH: 999, BOTTOM: 999, TOP: 999, CONF_INT_LOWER: 999, CONF_INT_UPPER: 999,
+                STD_DEV: 999, STD_ERR: 999}
 
 
 def calculate_statistics_from_fit(kd, pcov, df):
